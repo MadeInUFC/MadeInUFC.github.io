9acd28224dee6689d453acc8f08ba64207cb1ef4
GustavoM7 <gustavosmarquesf@gmail.com>
2020-01-10 20:34:06 -0300

    roteiro 2

--- script/index.js
+++ script/index.js
@@ -33,7 +33,7 @@ const data = {
       titulo_origem: "URI",
       link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2176",
       nivel: 1,
-      status: false
+      status: true
     },
     
     {
@@ -65,7 +65,7 @@ const data = {
       titulo_origem: "URI",
       link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2812",
       nivel: 4,
-      status: false
+      status: true
     },
     
     {
@@ -89,10 +89,83 @@ const data = {
       titulo_origem: "URI",
       link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2152",
       nivel: 2,
-      status: false
+      status: true
     }]
 
-  }]
+  }, {
+    titulo: "AD-HOC",
+    problemas: [{
+      titulo: "Copa do Mundo",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2376",
+      nivel: 2,
+      status: false,
+    },
+    {
+      titulo: "Conta de Água",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2369",
+      nivel: 0,
+      status: false,
+    },
+    {
+      titulo: "Zero ou um",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1467",
+      nivel: 0,
+      status: false,
+    },
+    {
+      titulo: "Revisão de Contrato",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1120",
+      nivel: 0,
+      status: false,
+    },
+    {
+      titulo: "Coloque tijolos na parede",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1426",
+      nivel: 0,
+      status: false,
+    },
+    {
+      titulo: "Alarme despertador",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1103",
+      nivel: 3,
+      status: false,
+    },
+    {
+      titulo: "Cofrinhos da Vó Vitória",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2247",
+      nivel: 3,
+      status: false,
+    },
+    {
+      titulo: "Elevador",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2348",
+      nivel: 1,
+      status: false,
+    },
+    {
+      titulo: "Mini-Poker",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2298",
+      nivel: 0,
+      status: false,
+    },
+    {
+      titulo: "Semente",
+      titulo_origem: "URI",
+      link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2452",
+      nivel: 0,
+      status: false,
+    }
+  ]
+  },]
 };
 
 //Retorna uma lista de problemas de um determinado roteiro (a partir do index 1)
