4181b49f720383b465ccf014cbd0a2bb624fa0bc
GustavoM7 <gustavosmarquesf@gmail.com>
2020-01-10 23:39:43 -0300

    add problema 6, roteiro 1

--- script/index.js
+++ script/index.js
@@ -65,7 +65,7 @@ const data = {
       titulo_origem: "URI",
       link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1963",
       nivel: 1,
-      status: false
+      status: true
     },
     
     {
