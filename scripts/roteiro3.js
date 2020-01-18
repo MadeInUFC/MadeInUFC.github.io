6ef14660f9165a120537acf2ac39043f9109c408
GustavoM7 <gustavosmarquesf@gmail.com>
2020-01-18 19:04:42 -0300

    add roteiro 3

---
+++ scripts/roteiro3.js
@@ -0,0 +1,121 @@
+const roteiro3 = {
+    titulo: "Estrutura de dados e STL",
+    num: 3,
+    descricao: "Problemas com uso de estrutura de organização de dados (pilhas, filas, conjuntos, etc) e utilização de Standard Template Library de C e C++",
+    dificuldade: "média",
+    materiais_de_apoio: [
+        {
+            titulo: "C++ - Referência",
+            link: "http://www.cplusplus.com/reference/",
+        },
+  
+        {
+            titulo: "Artigo - STL (part 1) (inglês)",
+            link: "https://www.topcoder.com/community/competitive-programming/tutorials/power-up-c-with-the-standard-template-library-part-1/",
+        },
+
+        {
+            titulo: "Artigo - STL (inglês)",
+            link: "https://www.topcoder.com/community/competitive-programming/tutorials/power-up-c-with-the-standard-template-library-part-2/",
+        },
+
+        {
+            titulo: "introdução ao STL",
+            link: "http://sweet.ua.pt/joao.p.silva/stl.html",
+        },
+
+        {
+            titulo: "STL",
+            link: "http://www.inf.pucrs.br/~pinho/PRGSWB/STL/stl.html",
+        },
+
+        {
+            titulo: "Introdução ao STL",
+            link: "https://br.ccm.net/faq/10156-introducao-ao-stl-em-c-standard-template-library",
+        },
+
+    ],
+    problemas: [
+      {
+        titulo: "Ele está impedido",
+        titulo_origem: "URI",
+        link_origem: " https://www.urionlinejudge.com.br/judge/pt/problems/view/1410",
+        nivel: 0,
+        status: false,
+      },
+
+      {
+        titulo: "Quem vai ser reprovado?",
+        titulo_origem: "URI",
+        link_origem: " https://www.urionlinejudge.com.br/judge/pt/problems/view/2134",
+        nivel: 0,
+        status: false,
+      },
+
+      {
+        titulo: "Quem vai ser reprovado?",
+        titulo_origem: "URI",
+        link_origem: " https://www.urionlinejudge.com.br/judge/pt/problems/view/2134",
+        nivel: 0,
+        status: false,
+      },
+
+      {
+        titulo: "Proteja sua senha",
+        titulo_origem: "URI",
+        link_origem: " https://www.urionlinejudge.com.br/judge/pt/problems/view/2287",
+        nivel: 0,
+        status: false,
+      },
+
+      {
+        titulo: "Troca de cartas",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1104",
+        nivel: 0,
+        status: false,
+      },
+
+      {
+        titulo: "Mário",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1408",
+        nivel: 0,
+        status: false,
+      },
+
+      {
+        titulo: "Triângulos",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1472",
+        nivel: 0,
+        status: false,
+      },
+
+      {
+        titulo: "Grid de Largada",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1228",
+        nivel: 0,
+        status: false,
+      },
+
+      {
+        titulo: "Subsequências",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1507",
+        nivel: 0,
+        status: false,
+      },
+
+      {
+        titulo: "Trilhos",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1062",
+        nivel: 0,
+        status: false,
+      },
+      
+    ]
+  }
+  
