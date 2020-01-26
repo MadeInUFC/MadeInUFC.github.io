68a08912d7f5c0858b6054af64eb297a61ee772b
GustavoM7 <gustavosmarquesf@gmail.com>
2020-01-25 23:20:21 -0300

    add roteiro 4

---
+++ scripts/roteiro4.js
@@ -0,0 +1,135 @@
+const roteiro4 = {
+  titulo: "Divisão e conquista e força bruta",
+  num: 4,
+  descricao: "Problemas com uso de estrutura de organização de dados (pilhas, filas, conjuntos, etc) e utilização de Standard Template Library de C e C++",
+  dificuldade: "alta",
+  materiais_de_apoio: [
+    {
+      titulo: "Unicamp - Algoritmos e programação de computadores (PDF)",
+      link: "http://www.ic.unicamp.br/~zanoni/mc102/2013-1s/aulas/aula22.pdf",
+    },
+
+    {
+      titulo: "Short Tutorial: Backtracking With Recursion",
+      link: "http://compsci.ca/v3/viewtopic.php?t=21497",
+    },
+
+    {
+      titulo: "Artigo: Introdução a Recursão - Parte 1 (inglês)",
+      link: "https://www.topcoder.com/community/competitive-programming/tutorials/an-introduction-to-recursion-part-1/",
+    },
+
+    {
+      titulo: "Artigo: Introdução a Recursão - Parte 2 (inglês)",
+      link: "https://www.topcoder.com/community/competitive-programming/tutorials/an-introduction-to-recursion-part-2/",
+    },
+
+    ],
+    problemas: [
+      {
+        titulo: "Caça ao Tesouro",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/2398",
+        nivel: 2,
+        status: false,
+        descricao_problema: "",
+        descricao_solucao: "",
+        analise: "",
+        implementacao: "https://github.com/MadeInUFC/MadeInUFC.github.io/blob/master/roteiros/roteiro4/problemas/problema1/resposta.cpp",
+        pseudocodigo:[
+          "&emsp; ",          
+        ]
+      },
+
+      {
+        titulo: "Polícia e Ladrão",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1905",
+        nivel: 5,
+        status: false,
+        descricao_problema: "",
+        descricao_solucao: "",
+        analise: "",
+        implementacao: "https://github.com/MadeInUFC/MadeInUFC.github.io/blob/master/roteiros/roteiro4/problemas/problema2/resposta.cpp",
+        pseudocodigo:[
+          "&emsp; ",          
+        ]
+      },
+
+      {
+        titulo: "Código genético",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1682",
+        nivel: 5,
+        status: false,
+        descricao_problema: "",
+        descricao_solucao: "",
+        analise: "",
+        implementacao: "https://github.com/MadeInUFC/MadeInUFC.github.io/blob/master/roteiros/roteiro4/problemas/problema3/resposta.cpp",
+        pseudocodigo:[
+          "&emsp; ",          
+        ]
+      },
+
+      {
+        titulo: "MegaDamas",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1404",
+        nivel: 7,
+        status: false,
+        descricao_problema: "",
+        descricao_solucao: "",
+        analise: "",
+        implementacao: "https://github.com/MadeInUFC/MadeInUFC.github.io/blob/master/roteiros/roteiro4/problemas/problema4/resposta.cpp",
+        pseudocodigo:[
+          "&emsp; ",          
+        ]
+      },
+
+      {
+        titulo: "Bolhas e Baldes",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1404",
+        nivel: 7,
+        status: false,
+        descricao_problema: "",
+        descricao_solucao: "",
+        analise: "",
+        implementacao: "https://github.com/MadeInUFC/MadeInUFC.github.io/blob/master/roteiros/roteiro4/problemas/problema5/resposta.cpp",
+        pseudocodigo:[
+          "&emsp; ",          
+        ]
+      },
+
+      {
+        titulo: "Calouro vence vetereno?",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1892",
+        nivel: 9,
+        status: false,
+        descricao_problema: "",
+        descricao_solucao: "",
+        analise: "",
+        implementacao: "https://github.com/MadeInUFC/MadeInUFC.github.io/blob/master/roteiros/roteiro4/problemas/problema6/resposta.cpp",
+        pseudocodigo:[
+          "&emsp; ",          
+        ]
+      },
+
+      {
+        titulo: "O problema da linha do horizonte",
+        titulo_origem: "URI",
+        link_origem: "https://www.urionlinejudge.com.br/judge/pt/problems/view/1576",
+        nivel: 9,
+        status: false,
+        descricao_problema: "",
+        descricao_solucao: "",
+        analise: "",
+        implementacao: "https://github.com/MadeInUFC/MadeInUFC.github.io/blob/master/roteiros/roteiro4/problemas/problema7/resposta.cpp",
+        pseudocodigo:[
+          "&emsp; ",          
+        ]
+      },
+      
+    ]
+  }
